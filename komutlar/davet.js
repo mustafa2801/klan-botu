f65bf38ca8b4cc554276b7fded0978712c42dd56
mustafa2801 <54547295+mustafa2801@users.noreply.github.com>
2019-10-13 11:19:27 +0300

    Create davet.js

---
+++ komutlar/davet.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message) => {
+  if (message.channel.type !== 'dm') {
+    const ozelmesajkontrol = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Özel mesajlarını kontrol et. :postbox:');
+    message.channel.sendEmbed(ozelmesajkontrol) }
+	const pingozel = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .setDescription('Sunucu Davet Linkimiz: https://discord.gg/h34Wm3d');
+    return message.author.sendEmbed(pingozel)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sunucudavet', 'invite'],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'davet',
+  description: 'Botun davet linkini gönderir.',
+  usage: 'davet'
+};
